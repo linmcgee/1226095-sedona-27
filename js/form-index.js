6eb01416d7ed35a3351b63e077ef169f2450529e
linmcgee <medi61200@mail.ru>
2020-03-20 20:03:09 +0300

    JS

--- js/form-index.js
+++ js/form-index.js
@@ -5,19 +5,29 @@ var arrivalDate = form.querySelector("[name=arrival-date]");
 var quantityAdults = form.querySelector("[name=adults]");
 var quantityChildren = form.querySelector("[name=children]");
 
-buttonSearchHotel.onclick = function(evt) {
+buttonSearchHotel.addEventListener("click", function(evt) {
 	form.classList.toggle("form-arrival-invisible");
 	form.classList.toggle("form-arrival-visible");
-	arrivalDate.focus();
-};
-
-form.onsubmit = function(evt) {
-	evt.preventDefault();
 	form.classList.remove("form-error");
-	if (!arrivalDate.value || !quantityAdults.value || !quantityChildren.value) {
-		form.classList.remove("form-error");
-		form.offsetWidth = form.offsetWidth;
-		form.classList.add("form-error");
-	}
-};
+	arrivalDate.focus();
+});
 
+  form.addEventListener("submit", function(evt) {
+    if (!arrivalDate.value || !quantityAdults.value || !quantityChildren.value) {
+      evt.preventDefault();
+      form.classList.remove("form-error");
+      form.offsetWidth = form.offsetWidth;
+      form.classList.add("form-error");
+    }
+  });
+  
+  window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (form.classList.contains("form-arrival-visible")) {
+		form.classList.remove("form-arrival-visible");
+		form.classList.add("form-arrival-invisible");
+        form.classList.remove("form-error");
+      }
+    }
+  });
