fac93eda975d3863232974dfbc1788dd5440a6ac
linmcgee <medi61200@mail.ru>
2020-03-21 22:22:57 +0300

    Выравнивание, удаление класса visible, переделка JS, добавление префиксов

--- js/form-index.js
+++ js/form-index.js
@@ -6,28 +6,28 @@ var quantityAdults = form.querySelector("[name=adults]");
 var quantityChildren = form.querySelector("[name=children]");
 
 buttonSearchHotel.addEventListener("click", function(evt) {
-  form.classList.toggle("form-arrival-visible");
-  if (form.classList.contains("form-arrival-visible")) {
-    arrivalDate.focus();
-  } else {
-    form.classList.remove("form-error");
-  }
+    form.classList.toggle("form-arrival-invisible");
+    if (form.classList.contains("form-arrival-invisible")) {
+        form.classList.remove("form-error");
+    } else {
+        arrivalDate.focus();
+    }
 });
 
 form.addEventListener("submit", function(evt) {
-  if (!arrivalDate.value || !quantityAdults.value || !quantityChildren.value) {
-    evt.preventDefault();
-    form.classList.remove("form-error");
-    form.offsetWidth = form.offsetWidth;
-    form.classList.add("form-error");
-  }
+    if (!arrivalDate.value || !quantityAdults.value || !quantityChildren.value) {
+        evt.preventDefault();
+        form.classList.remove("form-error");
+        form.offsetWidth = form.offsetWidth;
+        form.classList.add("form-error");
+    }
 });
 
 window.addEventListener("keydown", function(evt) {
-  if (evt.keyCode === 27) {
-    evt.preventDefault();
-    if (form.classList.contains("form-arrival-visible")) {
-      form.classList.remove("form-arrival-visible");
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (!form.classList.contains("form-arrival-invisible")) {
+            form.classList.add("form-arrival-invisible");
+        }
     }
-  }
 });
